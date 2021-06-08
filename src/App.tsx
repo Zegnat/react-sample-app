ee24da5e4c933a1687db75bbdf529e853127bdf0
Martijn van der Ven <martijn@vanderven.se>
2021-06-08 12:39:30 +0200

    Run prettier, no need to debate style

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,39 @@
-import * as React from 'react';
-import { analyzeText } from './../analysis'
-import { render } from 'react-dom';
-import Form from './Form'
-import Loader from './Loader'
-import Result from './Result'
+import * as React from "react";
+import { analyzeText } from "./../analysis";
+import { render } from "react-dom";
+import Form from "./Form";
+import Loader from "./Loader";
+import Result from "./Result";
 
 const App: React.FC = () => {
-    enum State {
-        WaitingForInput,
-        Loading,
-        ShowingResult
-    }
+  enum State {
+    WaitingForInput,
+    Loading,
+    ShowingResult,
+  }
 
-    const textField = React.useRef<HTMLTextAreaElement>();
+  const textField = React.useRef<HTMLTextAreaElement>();
 
-    const [state, setState] = React.useState(
-        State.WaitingForInput
+  const [state, setState] = React.useState(State.WaitingForInput);
+
+  const [results, setResults] = React.useState({ numWords: 0, numLetters: 0 });
+
+  const analyse = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    setState(State.Loading);
+    setResults(await analyzeText(textField.current.value));
+    setState(State.ShowingResult);
+  };
+
+  if (state === State.Loading) {
+    return <Loader />;
+  } else if (state === State.ShowingResult) {
+    return (
+      <Result numWords={results.numWords} numLetters={results.numLetters} />
     );
+  } else {
+    return <Form ref={textField} onSubmit={analyse} />;
+  }
+};
 
-    const [results, setResults] = React.useState({ numWords: 0, numLetters: 0 });
-
-    const analyse = async (event: React.FormEvent<HTMLFormElement>) => {
-        event.preventDefault();
-        setState(State.Loading);
-        setResults(await analyzeText(textField.current.value));
-        setState(State.ShowingResult);
-    }
-
-    if (state === State.Loading) {
-        return <Loader />
-    } else if (state === State.ShowingResult) {
-        return <Result numWords={results.numWords} numLetters={results.numLetters} />
-    } else {
-        return <Form ref={textField} onSubmit={analyse} />
-    }
-}
-
-render(<App />, document.body.firstElementChild)
+render(<App />, document.body.firstElementChild);
--- src/Result.tsx
+++ src/Result.tsx
@@ -1,16 +1,21 @@
-import * as React from 'react';
-import Box from '@material-ui/core/Box'
-import Typography from '@material-ui/core/Typography'
+import * as React from "react";
+import Box from "@material-ui/core/Box";
+import Typography from "@material-ui/core/Typography";
 
 interface ResultValues {
-    numWords: number;
-    numLetters: number;
+  numWords: number;
+  numLetters: number;
 }
 
 const Result: React.FC<ResultValues> = (results) => {
-    return <Box textAlign="center" marginTop={8}>
-        <Typography>Your text consists of {results.numWords} words ({results.numLetters} letters)</Typography>
+  return (
+    <Box textAlign="center" marginTop={8}>
+      <Typography>
+        Your text consists of {results.numWords} words ({results.numLetters}{" "}
+        letters)
+      </Typography>
     </Box>
-}
+  );
+};
 
 export default Result;
