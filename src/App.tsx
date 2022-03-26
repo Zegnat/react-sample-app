7a9540c7bbb5cbcee1efddecb6f0a8a512243370
Martijn van der Ven <martijn@vanderven.se>
2022-03-26 22:33:25 +0100

    Run App in StrictMode, move some more imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,38 @@
-import * as React from "react";
 import { analyzeText } from "./../analysis";
 import { render } from "react-dom";
 import { Form } from "./Form";
 import Loader from "./Loader";
-import Result from "./Result";
+import { Result } from "./Result";
+import { FormEvent, StrictMode, useState } from "react";
 
-const App: React.FC = () => {
+const App = () => {
   enum State {
     WaitingForInput,
     Loading,
     ShowingResult,
   }
 
-  const [state, setState] = React.useState(State.WaitingForInput);
+  const [state, setState] = useState(State.WaitingForInput);
 
-  const [results, setResults] = React.useState({ numWords: 0, numLetters: 0 });
+  const [results, setResults] = useState({ numWords: 0, numLetters: 0 });
 
-  const analyse = async (
-    event: React.FormEvent<HTMLFormElement>,
-    value: string
-  ) => {
+  const analyse = async (event: FormEvent<HTMLFormElement>, value: string) => {
     event.preventDefault();
     setState(State.Loading);
     setResults(await analyzeText(value));
     setState(State.ShowingResult);
   };
 
+  let appState = <Form onSubmit={analyse} />;
   if (state === State.Loading) {
-    return <Loader />;
+    appState = <Loader />;
   } else if (state === State.ShowingResult) {
-    return (
+    appState = (
       <Result numWords={results.numWords} numLetters={results.numLetters} />
     );
-  } else {
-    return <Form onSubmit={analyse} />;
   }
+
+  return <StrictMode>{appState}</StrictMode>;
 };
 
 render(<App />, document.body.firstElementChild);
