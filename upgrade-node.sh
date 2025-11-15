e2b213b94902bc1efbc0ce83fc6c1068a28358cb
Martijn van der Ven <martijn@vanderven.se>
2025-11-15 14:55:07 +0100

    No more dependency on gcrane

--- upgrade-node.sh
+++ upgrade-node.sh
@@ -3,14 +3,19 @@ set -euo pipefail
 
 IMAGE=gcr.io/distroless/nodejs24-debian12:nonroot
 
-# Use gcrane to request the exact digest of the latest Distroless LTS Node.js
-DIGEST=$(docker run --rm gcr.io/go-containerregistry/gcrane digest ${IMAGE} 2>/dev/null)
+echo -n "Pulling image: "
+docker image pull --quiet ${IMAGE}
+
+# Use docker inspect to get the exact tag and digest of the latest Distroless LTS Node.js
+REPO_TAG=$(docker image inspect --format='{{index .RepoTags 0}}' ${IMAGE})
+REPO_DIGEST=$(docker image inspect --format='{{index .RepoDigests 0}}' ${IMAGE})
+DIGEST="${REPO_TAG}@${REPO_DIGEST##*@}"
 
 # Get the exact version of Node.js from the container
-VERSION=$(docker run --rm ${IMAGE}@${DIGEST} "-v")
+VERSION=$(docker container run --pull never --rm ${DIGEST} "-v")
 
 fnm use --install-if-missing ${VERSION}
 echo "Distroless digest: ${DIGEST}"
 npm pkg set "engines.node"="${VERSION:1}"
 sed -E -i "" "s|node:[^-]+-bookworm|node:${VERSION:1}-bookworm|" Dockerfile
-sed -E -i "" "s|FROM .+ AS final|FROM ${IMAGE}@${DIGEST} AS final|" Dockerfile
+sed -E -i "" "s|FROM .+ AS final|FROM ${DIGEST} AS final|" Dockerfile
